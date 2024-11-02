b4bed41e1417919aa2034a8abd4a6cee13bcc2f2
Alan Liu <liualan998@gmail.com>
2024-11-02 15:45:36 -0400

    Add environment class

--- backend/app.py
+++ backend/app.py
@@ -1,19 +1,24 @@
 # builtin
+from contextlib import asynccontextmanager
 
 # external
 from fastapi import FastAPI
 from pydantic import BaseModel
+
+# internal
+from src.globals import Environment
+
 
 class User_Login(BaseModel):
     name: str 
     username: str #Email
     password: str 
     courses: str 
-    
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    environment: Environment = Environment()
+    app.state.environment = environment
+    yield
 
-
-# internal
-
-
-app = FastAPI()
+app: FastAPI = FastAPI(lifespan=lifespan)
