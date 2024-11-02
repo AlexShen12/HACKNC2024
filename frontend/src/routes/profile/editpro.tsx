91810cfe3e2943ae43f3bc3ccd8726bb2d04e339
alshen12 <ashen006@gmail.com>
2024-11-02 18:48:31 -0400

    modified:   frontend/src/routes/profile/editpro.tsx

--- frontend/src/routes/profile/editpro.tsx
+++ frontend/src/routes/profile/editpro.tsx
@@ -2,24 +2,15 @@
 
 
 
-export default function EditProfile(){
+export default function EditProfile() {
     <div>
         <form>
             <div>
                 <label htmlFor='name'>Profile: </label>
-                    <input
-                        id='name'
-                        name='name'
-                    />
-                </div>
-                <div>
-                    <label htmlFor='courses'>Courses: </label>
-                    <input
-                        id='courses'
-                        name='courses'
-                        type="hidden"
-                        value={}
-                    />
+                <input
+                    id='name'
+                    name='name'
+                />
             </div>
             <button type='submit'> Login </button>
         </form>
